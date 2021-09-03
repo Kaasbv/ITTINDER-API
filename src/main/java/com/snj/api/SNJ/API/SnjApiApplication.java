9851819afdc70926a24ff2d43b88da9b4aaa2adf
ExploHash <jbtwaalf@gmail.com>
2021-09-03 17:33:05 +0200

    Added stuff

---
+++ src/main/java/com/snj/api/SNJ/API/SnjApiApplication.java
@@ -0,0 +1,13 @@
+package com.snj.api.SNJ.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnjApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnjApiApplication.class, args);
+	}
+
+}
