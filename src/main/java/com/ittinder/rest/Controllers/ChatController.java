e4ae375d6a1cfc5ecb849d97167f2fc6fcf407bf
ExploHash <jbtwaalf@gmail.com>
2021-10-10 12:18:42 +0200

    Added post calls and some fixes

---
+++ src/main/java/com/ittinder/rest/Controllers/ChatController.java
@@ -0,0 +1,55 @@
+package com.ittinder.rest.Controllers;
+
+import com.ittinder.rest.Entities.Chat;
+import com.ittinder.rest.Entities.Message;
+import com.ittinder.rest.Entities.User;
+import com.ittinder.rest.Repositories.ChatRepository;
+import com.ittinder.rest.Repositories.MessageRepository;
+import com.ittinder.rest.Repositories.UserRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
+@RestController
+public class ChatController {
+  private final ChatRepository chatRepository;
+  private final MessageRepository messageRepository;
+  private final UserRepository userRepository;
+
+  ChatController(ChatRepository repository, MessageRepository messageRepository, UserRepository userRepository) {
+    this.chatRepository = repository;
+    this.messageRepository = messageRepository;
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping("/chat/{id}/messages")
+  public List<Message> getChatMessages(@PathVariable Long id, @RequestParam String startDate, @RequestParam String endDate){
+    return messageRepository.findByCreatedDateBetweenAndChatId(startDate, endDate, id);
+  }
+
+  @PostMapping("/chat/{id}/messages")
+  public Message postMessage(@PathVariable Long id, @RequestBody String message){
+    User currentUser = userRepository.getById((long)1);
+    Chat chat = chatRepository.getById(id);
+    Message newMessage = new Message(currentUser, chat, message);
+    
+    messageRepository.save(newMessage);
+
+    return newMessage;
+  }
+
+  @DeleteMapping("/chat/{id}")
+  public ResponseEntity<HttpStatus> deleteChat(@PathVariable Long id){
+    chatRepository.deleteById(id);
+    return ResponseEntity.noContent().build();
+  }
+
+}
--- src/main/java/com/ittinder/rest/Repositories/MessageRepository.java
+++ src/main/java/com/ittinder/rest/Repositories/MessageRepository.java
@@ -3,9 +3,8 @@ package com.ittinder.rest.Repositories;
 import com.ittinder.rest.Entities.Message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-  // @Query("select * from message where timestamp > ?1 AND timestamp < ?2")
-  // Object[] findByTimeCustomQuery(String dFrom, String dTo);
+  List<Message> findByCreatedDateBetweenAndChatId(String from, String to, Long chatId);
 }
