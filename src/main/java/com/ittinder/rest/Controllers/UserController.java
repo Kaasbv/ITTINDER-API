0d6f6c1652536dc4d0ca4128cce012cd323d16ac
Nass1m <70798807+Nass1m@users.noreply.github.com>
2021-10-12 13:52:01 +0200

    IT-22-API Password validation and user registration

--- src/main/java/com/ittinder/rest/Controllers/UserController.java
+++ src/main/java/com/ittinder/rest/Controllers/UserController.java
@@ -4,10 +4,14 @@ import com.ittinder.rest.Entities.User;
 import com.ittinder.rest.Repositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.events.Event;
+
+import javax.validation.constraints.Email;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -18,17 +22,34 @@ public class UserController {
     this.userRepository = repository;
   }
 
-  //Postmapping multiple values from constructor in User class
-  @PostMapping( "/user")
+  //Creating a user
+  @PostMapping( "/users")
   public ResponseEntity<HttpStatus> createUser(@RequestBody User newUser) {
     User user = newUser;
-    userRepository.save(user);
-    return ResponseEntity.ok(HttpStatus.OK);
+    String email = user.getEmail();
+
+    if(userRepository.findByEmailIgnoreCase(email).isEmpty()) {
+      userRepository.save(user);
+      return ResponseEntity.ok(HttpStatus.CREATED);
+    } else {
+      return ResponseEntity.ok(HttpStatus.CONFLICT);
+    }
   }
 
-  @GetMapping()
-  public void getUser() {
-
+  //Get user by id
+  @GetMapping("/users")
+  public List<User> getUserById(@RequestParam(required = false) Long id) {
+    List<User> users = new ArrayList<>();
+    if (id == null) {
+      users.addAll(userRepository.findAll());
+    }
+    else {
+      users.addAll(userRepository.findUserById(id));
+    }
+    return users;
   }
+
 }
+
+
 
--- src/main/java/com/ittinder/rest/Entities/User.java
+++ src/main/java/com/ittinder/rest/Entities/User.java
@@ -1,50 +1,56 @@
 package com.ittinder.rest.Entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Size;
+import com.ittinder.rest.ValidPassword;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+@Getter
+@Setter
 @Entity
 public class User {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  //@NotEmpty(message = "First name can't be empty")
+  @NotEmpty(message = "First name can't be empty")
   @Size(min = 2, max = 15)
   private String firstName;
 
   private String middleName;
 
-  //@NotEmpty(message = "Last name can't be empty")
+  @NotEmpty(message = "Last name can't be empty")
   @Size(min = 2, max = 15)
+  @NotEmpty(message = "Last name can't be empty")
   private String surname;
 
-  @Past
+  @Past(message = "Date of birth must be in the past")
+  @NotNull(message = "Date of birth must be filled in")
   private LocalDate dateOfBirth;
 
-  //@NotEmpty
+  @NotEmpty(message = "Email can't be empty")
   @Email(message = "Email should be valid")
+  @Column(unique = true)
   private String email;
 
-  //Create custom annotation @ValidPassword
+  @ValidPassword(message = "Password must contain a special and uppercase character, and be at least eight characters long")
+  @NotEmpty(message = "Email can't be empty")
   private String password;
+  private String matchingPassword;
 
-  //@NotEmpty(message = "You have to fill in a gender")
+  @NotEmpty(message = "You have to fill in a gender")
   private String gender;
 
-  //@NotEmpty(message = "You have to be interested in a gender")
+  @NotEmpty(message = "You have to be interested in a gender")
   private String interestedInGender;
 
-  //@NotEmpty(message = "You have to fill in a description")
+  @NotEmpty(message = "You have to fill in a description")
   @Size(min = 10, max = 256, message = "Description must be between 10 and 256 characters")
   private String description;
 
@@ -53,135 +59,32 @@ public class User {
   private double longitude;
   private String currentLocation;
 
-  // constructor
-  // Nog niet zeker welke velden mee worden genomen in defitinieve constructor
-  public User(String firstName, String surname, String email) {
+  // Constructor
+  public User(String firstName,
+              String middleName,
+              String surname,
+              LocalDate dateOfBirth,
+              String email,
+              String password,
+              String matchingPassword,
+              String gender,
+              String interestedInGender,
+              String description) {
     this.firstName = firstName;
-    this.surname = surname;
-    this.email = email;
-  }
-
-  // overloading constructor
-  public User(String firstName) {
-    this.firstName = firstName;
-  }
-
-
-  /****************GETTERS****************/
-  public Long getId() {
-    return id;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getMiddleName() {
-    return middleName;
-  }
-
-  public String getSurname() {
-    return surname;
-  }
-
-  public LocalDate getDateOfBirth() {
-    return dateOfBirth;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public String getCurrentLocation() {
-    return currentLocation;
-  }
-
-  public String getGender() {
-    return gender;
-  }
-
-  public LocalDateTime getLastLogin() {
-    return lastLogin;
-  }
-
-  public double getLatitude() {
-    return latitude;
-  }
-
-  public double getLongitude() {
-    return longitude;
-  }
-
-  public String getInterestedInGender() {
-    return interestedInGender;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-  /****************END OF GETTERS****************/
-
-  /****************SETTERS****************/
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public void setMiddleName(String middleName) {
     this.middleName = middleName;
-  }
-
-  public void setSurname(String surname) {
     this.surname = surname;
-  }
-
-  public void setDateOfBirth(LocalDate dateOfBirth) {
     this.dateOfBirth = dateOfBirth;
-  }
-
-  public void setEmail(String email) {
     this.email = email;
-  }
-
-  public void setPassword(String password) {
     this.password = password;
-  }
-
-  public void setCurrentLocation(String currentLocation) {
-    this.currentLocation = currentLocation;
-  }
-
-  public void setGender(String gender) {
+    this.matchingPassword = matchingPassword;
     this.gender = gender;
-  }
-
-  public void setLastLogin(LocalDateTime lastLogin) {
-    this.lastLogin = lastLogin;
-  }
-
-  public void setLatitude(double latitude) {
-    this.latitude = latitude;
-  }
-
-  public void setLongitude(double longitude) {
-    this.longitude = longitude;
-  }
-
-  public void setInterestedInGender(String interestedInGender) {
     this.interestedInGender = interestedInGender;
-  }
-
-  public void setDescription(String description) {
     this.description = description;
   }
-  /****************END OF SETTERS****************/
+
+  public User() {
+  }
+
 
   @Override
   public String toString() {
